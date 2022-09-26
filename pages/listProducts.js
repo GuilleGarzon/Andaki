6b9f141485b7121eeb80ec6af6cda1ce05b9d7d8
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-09-26 08:48:04 -0500

    feat: fix styles in listProducts pages

--- pages/listProducts.js
+++ pages/listProducts.js
@@ -8,7 +8,7 @@ export default function ListProducts({ products }) {
 
   return (
     <Layout title="Products">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 mt-10 mb-10">
         {products.map((product) => (
           <ProductItem
             product={product}
