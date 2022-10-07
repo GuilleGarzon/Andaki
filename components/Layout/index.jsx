c674e9f7e3a3365e986f223bc888e0e0e3f95430
Guillermo Garzón <91085346+GuilleGarzon@users.noreply.github.com>
2022-10-07 17:23:47 -0500

    Develop -> Main (#4)
    * feat: add component Layout and tailwind styles in global.css
    
    * add Component Footer
    
    * fix component Layout
    
    * feat: add page Login
    
    * feat: add page register
    
    * fix styles in component Layout
    
    * feat: add home page
    
    * feat: fix styles in componnet Layout
    
    * fix styles in component Layout
    
    * feat: add pages abut products
    
    * feat: add styles and configuration
    
    * add file .env.example
    
    * feat: fix redirec paget login
    
    * add auth in page
    
    * feat: add images
    
    * git add logo Andaki Karts
    
    * feat: add store of context
    
    * add page unauthorized
    
    * feat: add page product item with slug
    
    * feat: add page cart and fix page product
    
    * add adminPage about users
    
    * add admin page about products
    
    * feat: add endpoints about auth, admin and products
    
    * feat: add Adminpage about products
    
    * feat: Inclide store and paypal in page _app
    
    * feat: add page AdminOrders and fix others pages of Admin
    
    * feat: add models about Order, products and users
    
    * feat: add file database and page Profile
    
    * feat: fix styles in component Layout
    
    * feat: fix styles in pages/cart
    
    * feat: add pay
    
    * feat: fix styles in Adminpages
    
    * feat: fix styles in listProducts pages
    
    * feat: rename files in pages and components
    
    * feat: fix styles in page order-history
    
    * feat: fix styles in pages
    
    * feat: add alerts for confirmation in delete products
    
    * feat: add alerts in page AdminUsers
    
    * feat: fix styles in pages of pay
    
    * feat: fix paypal USD in page Pay
    
    * feat: delete files repeat
    
    * feat: fix component Layout
    
    * feat: fix names in pages
    
    * feat: fix logout
    
    * feat: fix logout
    
    * feat: add styles in component Layout
    
    * feat: fix styles about Navbar

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,31 +1,11 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { useEffect, useState, useContext } from 'react';
-import { Store } from '../../store/index';
 import { ToastContainer } from 'react-toastify';
-import { signOut, useSession } from 'next-auth/react';
 import 'react-toastify/dist/ReactToastify.css';
-import { Menu } from '@headlessui/react';
-import DropdownLink from '../DropdownLink';
-import Cookies from 'js-cookie';
 import Footer from '../Footer/index';
-import Image from 'next/image';
+import Header from '../NavBar/index'
 
-function Layout({ title, children }) {
-  const { status, data: session } = useSession();
+function Layout({ title, children }) {      
 
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
-  const [cartItemsCount, setCartItemsCount] = useState(0);
-  const router = useRouter();
-  const { redirect } = router.query;
-  useEffect(() => {
-    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
-  }, [cart.cartItems]);
-
-  const logoutClickHandler = () => {
-    signOut({ redirect: false, callbackUrl: '/' });
-  };
   return (
     <>
       <Head>
@@ -37,123 +17,8 @@ function Layout({ title, children }) {
       <ToastContainer position="bottom-center" limit={1} />
 
       <div className="flex min-h-screen flex-col justify-between ">
-        <header>
-          <nav className="flex h-20 items-center px-4 justify-between shadow-md sm:px-12 bg-red-600">
-            {session?.user ? (
-              <Link href="/listProducts">
-                <a className="flex justify-center items-center">
-                  <Image
-                    src="/LogoAndaki.png"
-                    alt="Logo Andaki Karts"
-                    width={60}
-                    height={60}
-                    className="rounded-full"
-                  />
-                </a>
-              </Link>
-            ) : (
-              <Link href="/">
-                <a className="flex justify-center items-center">
-                  <Image
-                    src="/LogoAndaki.png"
-                    alt="Logo Andaki Karts"
-                    width={60}
-                    height={60}
-                    className="rounded-full"
-                  />
-                </a>
-              </Link>
-            )}
-            <div className="flex justify-center text-center ">
-              <Link href="/cart">
-                <a className="p-3 text-white font-bold hover:text-gray-200">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                    />
-                  </svg>
-
-                  {cartItemsCount > 0 && (
-                    <span className="rounded-full justify-center absolute -mt-10 bg-blue-600 px-2 py-1 text-xs font-bold text-white">
-                      {cartItemsCount}
-                    </span>
-                  )}
-                </a>
-              </Link>
-
-              {status === 'loading' ? null : session?.user ? (
-                <Menu as="div" className="relative inline-block">
-                  <Menu.Button className="text-white font-bold p-3 hover:text-gray-200">
-                    {session.user.name}
-                  </Menu.Button>
-                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white shadow-lg">
-                    <Menu.Item>
-                      <DropdownLink className="dropdown-link" href="/profile">
-                        Perfil
-                      </DropdownLink>
-                    </Menu.Item>
-                    {session.user.isAdmin ? (
-                      <Menu.Item>
-                        <DropdownLink
-                          className="dropdown-link"
-                          href="/listProducts"
-                        >
-                          Lista de Productos
-                        </DropdownLink>
-                      </Menu.Item>
-                    ) : (
-                      <Menu.Item>
-                        <DropdownLink
-                          className="dropdown-link"
-                          href="/order-history"
-                        >
-                          Historial de Ordenes
-                        </DropdownLink>
-                      </Menu.Item>
-                    )}
-
-                    {session.user.isAdmin && (
-                      <Menu.Item>
-                        <DropdownLink
-                          className="dropdown-link"
-                          href="/admin/orders"
-                        >
-                          Tablero Admin
-                        </DropdownLink>
-                      </Menu.Item>
-                    )}
-
-                    <Menu.Item>
-                      <a
-                        className="dropdown-link"
-                        href="/"
-                        onClick={logoutClickHandler}
-                      >
-                        Cerrar Sesión
-                      </a>
-                    </Menu.Item>
-                  </Menu.Items>
-                </Menu>
-              ) : (
-                <Link href="/login">
-                  <a className="p-2 sm:text-lg text-white font-bold hover:text-gray-200">
-                    Iniciar Sesión
-                  </a>
-                </Link>
-              )}
-            </div>
-          </nav>
-        </header>
-        <main className="container m-auto mt-0">{children}</main>
+        <Header />
+        <main className="container mx-auto flex">{children}</main>       
         <Footer />
       </div>
     </>
